c14b2957469377070f9d13fbd68541e510a57be3
alaa5090 <alaaabdraboo297@gmail.com>
2021-12-20 19:15:38 +0200

    list all cats

--- src/models.rs
+++ src/models.rs
@@ -3,6 +3,10 @@ use super::schema::cats;
 #[derive(Queryable, Serialize)]
 pub struct Cat {
 pub id:i32,
-pub name: string,
-pub image_path:String 
+pub name: String,
+pub image_path:String,
+}
+pub struct IndexTemplateData{
+    project_name:String,
+    cats:Vec<Cat>,
 }
